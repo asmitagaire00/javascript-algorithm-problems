54ea2f73fc3629ebc2d98903f7b7f2feb4bf39d8
asmitagaire00 <asmitagaire00@gmail.com>
2021-10-12 09:17:49 +1100

    Solved search and replace problem.

---
+++ search-and-replace.js
@@ -0,0 +1,24 @@
+// Search and ReplacePassed
+// Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+
+// First argument is the sentence to perform the search and replace on.
+
+// Second argument is the word that you will be replacing (before).
+
+// Third argument is what you will be replacing the second argument with (after).
+
+// Note: Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word Book with the word dog, it should be replaced as Dog
+
+function myReplace(str, before, after) {
+  let afterAfter;
+  const character = before[0];
+  if (character == character.toUpperCase()) {
+    afterAfter = after[0].toUpperCase() + after.substring(1, after.length);
+    return str.replace(before, afterAfter);
+  } else if (character == character.toLowerCase()) {
+    afterAfter = after[0].toLowerCase() + after.substring(1, after.length);
+    return str.replace(before, afterAfter);
+  }
+}
+
+myReplace('A quick brown fox Jumped over the lazy dog', 'Jumped', 'Leaped');
