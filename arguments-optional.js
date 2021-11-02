00ebf78aa94c7270e0af27530d567bb24ee382bb
asmitagaire00 <asmitagaire00@gmail.com>
2021-11-02 22:04:31 +1100

    completed javascript-algorithm-problems

--- arguments-optional.js
+++ arguments-optional.js
@@ -11,7 +11,16 @@
 // If either argument isn't a valid number, return undefined.
 
 function addTogether() {
-  return false;
+  const [first, second] = arguments;
+  if (typeof first !== 'number') {
+    return undefined;
+  }
+  if (second === undefined) {
+    return (second) => addTogether(first, second);
+  } else if (typeof second !== 'number') {
+    return undefined;
+  }
+  return first + second;
 }
 
-addTogether(2, 3);
+console.log(addTogether(5)(7));
--- smallest-common-multiple.js
+++ smallest-common-multiple.js
@@ -6,8 +6,42 @@
 
 // For example, if given 1 and 3, find the smallest common multiple of both 1 and 3 that is also evenly divisible by all numbers between 1 and 3. The answer here would be 6.
 
+//lcm for array.
 function smallestCommons(arr) {
-  return arr;
+  let newArr = [];
+  let min = arr[0] < arr[1] ? arr[0] : arr[1];
+  let max = arr[0] > arr[1] ? arr[0] : arr[1];
+  for (let i = min; i <= max; i++) {
+    newArr.push(i);
+  }
+
+  function gcd(a, b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+  }
+
+  let result = newArr[0];
+  for (let i = 1; i < newArr.length; i++) {
+    result = (result * newArr[i]) / gcd(newArr[i], result);
+  }
+
+  return result;
 }
 
-smallestCommons([1, 5]);
+console.log(smallestCommons([1, 5]));
+
+// lcm for only two numbers.
+// function smallestCommons(arr) {
+//   let min = (arr[0]<arr[1])?arr[0]:arr[1];
+//   let max = (arr[0]>arr[1])?arr[0]:arr[1];
+
+//   let hcf, lcm;
+//   for (let i = 1; i <= num1 && i <= num2; i++) {
+//     if (num1 % i === 0 && num2 % i === 0) {
+//       hcf = i;
+//     }
+//   }
+//   return (lcm = (num1 * num2) / hcf);
+// }
+
+// console.log(smallestCommons(1, 5));
